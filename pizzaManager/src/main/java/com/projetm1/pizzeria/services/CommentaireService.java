7b97da7c1b2e72b26ef6c94c2c504ae742f2d0b3
elebras1 <erwan.lebras27@gmail.com>
2025-03-12 15:27:42 +0100

    add services

--- pizzaManager/src/main/java/com/projetm1/pizzeria/services/CommentaireService.java
+++ pizzaManager/src/main/java/com/projetm1/pizzeria/services/CommentaireService.java
@@ -1,5 +1,6 @@
 package com.projetm1.pizzeria.services;
 
+import com.projetm1.pizzeria.repository.mongo.CommentaireRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
--- pizzaManager/src/main/java/com/projetm1/pizzeria/services/CompteService.java
+++ pizzaManager/src/main/java/com/projetm1/pizzeria/services/CompteService.java
@@ -1,9 +1,54 @@
 package com.projetm1.pizzeria.services;
 
+import com.projetm1.pizzeria.dtos.CompteDto;
+import com.projetm1.pizzeria.entities.Compte;
+import com.projetm1.pizzeria.mappers.CompteMapper;
+import com.projetm1.pizzeria.repository.jpa.CompteRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service("compteService")
 @Transactional
 public class CompteService {
+    private final CompteRepository compteRepository;
+    private final CompteMapper compteMapper;
+
+    public CompteService(CompteRepository compteRepository, CompteMapper compteMapper) {
+        this.compteRepository = compteRepository;
+        this.compteMapper = compteMapper;
+    }
+
+    public CompteDto getCompteById(Long id) {
+        return compteMapper.toDto(this.compteRepository.findById(id).orElse(null));
+    }
+
+    public List<CompteDto> getAllCompte() {
+        List<CompteDto> compteDtos = new ArrayList<>();
+        for(Compte compte : this.compteRepository.findAll()) {
+            compteDtos.add(this.compteMapper.toDto(compte));
+        }
+
+        return compteDtos;
+    }
+
+    public CompteDto saveCompte(CompteDto compteDto) {
+        Compte compte = this.compteMapper.toEntity(compteDto);
+        compte = this.compteRepository.save(compte);
+
+        return this.compteMapper.toDto(compte);
+    }
+
+    public void deleteCompteById(Long id) {
+        this.compteRepository.deleteById(id);
+    }
+
+    public CompteDto updateCompte(CompteDto compteDto) {
+        Compte compte = this.compteMapper.toEntity(compteDto);
+        compte = this.compteRepository.save(compte);
+
+        return this.compteMapper.toDto(compte);
+    }
 }
