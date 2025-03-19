c7b775128ccccc170cbe7643297ea930a7b08c9d
ByDefalt <rousvalromain@gmail.com>
2025-03-19 12:43:17 +0100

    add error

--- pizzaManager/src/main/java/com/projetm1/pizzeria/error/GlobalExceptionHandler.java
+++ pizzaManager/src/main/java/com/projetm1/pizzeria/error/GlobalExceptionHandler.java
@@ -1,7 +1,15 @@
 package com.projetm1.pizzeria.error;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler(BadRequest.class)
+    public ResponseEntity<String> handleInvalidPassword(BadRequest ex) {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.BAD_REQUEST);
+    }
 }
